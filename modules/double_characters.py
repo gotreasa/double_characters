2310d153c900fe6c6b964eb4908b4dc1614abb43
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-18 11:41:47 +0000

    feat: should give 'aa' for the input 'a'

--- modules/double_characters.py
+++ modules/double_characters.py
@@ -1,2 +1,4 @@
 def double_chars(string: str) -> str:
+    if string == "a":
+        return "aa"
     raise ValueError("❗️ Input should be a string")
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -7,3 +7,7 @@ def describe_double_char():
         """🧪 should give an error message when the input is not a string"""
         with pytest.raises(ValueError, match="❗️ Input should be a string"):
             double_characters.double_chars(72)
+
+    def should_double_a_single_lowercase_letter():
+        """🧪 should give 'aa' for the input 'a'"""
+        assert double_characters.double_chars("a") == "aa"
